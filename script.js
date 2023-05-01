2980335ed664e08e141fb4e7a97d5d7ade1c3f5f
Aman Thakur <67583037+amanRT@users.noreply.github.com>
2023-05-02 00:22:49 +0530

    1-05.2023

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 ///////////////////////////////////////
 // Modal window
 
@@ -28,6 +30,37 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+///////////////////////////////////////
+//Button Scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+///////////////////////////////////////
+// PAGE NEVIGATION
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+// e.preventDefault();
+// const id = this.getAttribute('href');
+// document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+//EVENT DELEGATION
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log(e.target);
+  // MATCHING STRATEGY
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+// TABBED COMPONENT
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+///////////////////////////////////////
+//TESTING
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
@@ -48,22 +81,3 @@ document
 //Style
 // message.style.backgroundColor = '#37383d';
 // message.style.width = '120%';
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
-btnScrollTo.addEventListener('click', function (e) {
-  // const s1cords = section1.getBoundingClientRect();
-  // window.scrollTo({
-  //   left: s1cords.left + window.pageXOffset,
-  //   top: s1cords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
-const h1 = document.querySelector('h1');
-// h1.addEventListener('mouseenter', function (e) {
-//   alert('HEYYYYYY');
-// });
-h1.onmouseenter = function () {
-  alert('HO HA HO HA HO HA');
-};
